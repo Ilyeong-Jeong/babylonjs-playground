35888971ed1044cdc4e6db35cbce8e6da0bf9701
ilyeongJeong <developjiy@gmail.com>
2021-08-19 00:03:14 +0900

    ref #1 babylonjs 기본 셋팅

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # babylonjs-playground
-Babylon.js playground
+**Babylon.js playground**
+
+## Usage
+**Install dependencies**
+```
+yarn
+```
+
+**Run development mode**
+```
+yarn dev
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,34 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect, useRef } from 'react'
+
+import * as BABYLON from 'babylonjs';
+import 'babylonjs-loaders';
+import * as BABYLONMaterials from 'babylonjs-materials';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvas = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    // babylon engine 및 scene 인스턴스화
+    const engine: BABYLON.Engine = new BABYLON.Engine(canvas.current, true);
+    const scene: BABYLON.Scene = new BABYLON.Scene(engine);
+
+    // Playground code start
+    // 큐브
+    const camera = new BABYLON.ArcRotateCamera('camera', 3 * Math.PI / 4, Math.PI / 4, 4, BABYLON.Vector3.Zero(), scene);
+    camera.attachControl(canvas, true);
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, 1, 0), scene);
+    const box = BABYLON.MeshBuilder.CreateBox('box', {height: 1, width: 1, depth: 1}, scene);
+    // Playground code end
+
+    // 렌더링
+    engine.runRenderLoop(() => {
+      scene!.render();
+    });
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <canvas ref={canvas} style={{ width:'100%', height: '100%' }}></canvas>
+  );
 }
 
-export default App
+export default App;
